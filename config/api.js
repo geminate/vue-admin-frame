e1865f957af7ec500e945862eaab9d92fcf347b1
liuhuihao <1061036802@qq.com>
2019-07-03 17:35:36 +0800

    feat: 添加系统管理

--- config/api.js
+++ config/api.js
@@ -1,6 +1,7 @@
 const api = {
     login: '/login',                                        // 登陆
     getUserInfo: '/getUserInfo',                            // 获取用户信息
+    getDepartmentTree: '/getDepartmentTree',                // 获取部门树
 };
 
 export default api;
--- src/pages/system/departmentManage/index.vue
+++ src/pages/system/departmentManage/index.vue
@@ -1,12 +1,34 @@
 <template>
     <div class="department-manage-page">
 
+
+        <el-tree :data="departmentTreeData" show-checkbox node-key="id" default-expand-all></el-tree>
+
     </div>
 </template>
 
 <script>
     export default {
         name: 'departmentManage',
+        data() {
+            return {
+                departmentTreeData: []
+            }
+        },
+        methods: {
+            // 初始化页面数据
+            async initValue() {
+                const {resultCode, resultMessage, data} = await this.$api.getDepartmentTree();
+                if (resultCode == 0) {
+                    this.departmentTreeData = data;
+                } else {
+                    this.$utils.error(resultMessage);
+                }
+            }
+        },
+        mounted() {
+            this.initValue();
+        }
     }
 </script>
 
