9e16254e9d0dde0830d78dac5869d9a88db4595a
liuhuihao <1061036802@qq.com>
2019-07-08 10:24:43 +0800

    feat: CURD 基本查询与分页

--- src/pages/curd/list/index.vue
+++ src/pages/curd/list/index.vue
@@ -2,7 +2,7 @@
     <div class="list-page">
 
         <!-- 筛选表单 -->
-        <el-form :inline="true" :model="filterData" class="filter-form" labelPosition="right" size="mini">
+        <el-form :inline="true" :model="filterData" labelPosition="right" size="mini">
             <el-form-item label="姓名">
                 <el-input v-model="filterData.name" placeholder="姓名"></el-input>
             </el-form-item>
@@ -17,19 +17,21 @@
             </el-form-item>
         </el-form>
 
-        <!-- 数据表格 -->
+        <!-- 表格上部的按钮 -->
         <div class="table-btn">
             <el-button size="mini" type="success">添加</el-button>
             <el-button size="mini" type="danger">多选删除</el-button>
         </div>
 
-        <el-table :data="tableData" border v-loading="tableLoading">
+        <!-- 数据表格 -->
+        <el-table :data="tableData" border @sort-change="sortChange" v-loading="tableLoading"
+                  :default-sort="{prop: orderBy, order: order}">
             <el-table-column type="selection" width="55" align="center"></el-table-column>
-            <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-            <el-table-column prop="sex" label="姓别" width="50"></el-table-column>
-            <el-table-column prop="date" label="日期" width="120"></el-table-column>
-            <el-table-column prop="address" label="地址"></el-table-column>
-            <el-table-column label="操作">
+            <el-table-column prop="name" sortable="custom" label="姓名" width="100"></el-table-column>
+            <el-table-column prop="sex" sortable="custom" label="姓别" width="80"></el-table-column>
+            <el-table-column prop="date" sortable="custom" label="日期" width="100"></el-table-column>
+            <el-table-column prop="address" sortable="custom" label="地址"></el-table-column>
+            <el-table-column label="操作" width="150">
                 <template slot-scope="scope">
                     <el-button size="mini">编辑</el-button>
                     <el-button size="mini" type="danger">删除</el-button>
@@ -37,15 +39,16 @@
             </el-table-column>
         </el-table>
 
-        <el-pagination background
+        <!-- 分页 -->
+        <el-pagination class="pagination"
+                       background
                        @size-change="changeQuery"
                        @current-change="changeQuery"
                        layout="total, sizes, prev, pager, next, jumper"
                        :current-page.sync="paginationData.pageNum"
                        :total="totalAmount"
                        :page-size.sync="paginationData.pageSize"
-                       :page-sizes="pageSizes"
-                       :style="{float:'right',marginTop:'20px'}">
+                       :page-sizes="pageSizes">
         </el-pagination>
     </div>
 </template>
@@ -64,8 +67,13 @@
                     pageNum: 0, // 当前页码
                     pageSize: 10, // 分页大小
                 },
+                sortData: {},
                 pageSizes: [10, 30, 50], // 分页大小选项
                 totalAmount: 0, // 数据总量
+
+                orderBy: null, // 排序列
+                order: '', // 排序方式
+
                 tableData: [], // 表格数据
                 tableLoading: false, // 表格是否正在加载
             }
@@ -91,6 +99,8 @@
                 const query = this.$route.query;
                 this.filterData = {name: query.name, date: query.date, address: query.address};
                 this.paginationData = {pageNum: Number(query.pageNum), pageSize: Number(query.pageSize)};
+                this.orderBy = query.orderBy;
+                this.order = query.order;
             },
 
             // 刷新表格数据
@@ -111,7 +121,20 @@
 
             // 改变页面地址参数
             changeQuery() {
-                this.$router.push({query: {...this.filterData, ...this.paginationData}});
+                this.$router.push({
+                    query: {
+                        ...this.filterData, ...this.paginationData,
+                        orderBy: this.orderBy,
+                        order: this.order
+                    }
+                });
+            },
+
+            // 排序方式发生变化
+            sortChange(sort) {
+                this.orderBy = sort.prop;
+                this.order = sort.order;
+                this.changeQuery();
             }
         },
         created() {
